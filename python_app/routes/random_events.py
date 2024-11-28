a2b3723cdb1223e863544435d50624bb8d3cf047
AverageKasper <averagekasper@gmail.com>
2024-11-28 23:43:35 +0200

    Random events more or less complete

--- python_app/routes/random_events.py
+++ python_app/routes/random_events.py
@@ -13,37 +13,44 @@ event_list = ["Sausage",
               "Celebrity",
               "Burning hand",
               "Poolparty",
-              "PDiddy",]
+              "PDiddy",
+              "lost_ticket"]
 
-
+@random_blueprint.route('/event')
 def random_event():
     money = 0
     carbon = 0
     shark = 0
+    lore = 0
     result = {}
     text_result = ""
     if len(event_list) == 0:
         result = {"event": False} 
         return jsonify(result)
-    event_check = r.randint(0,len(event_list)-1)
+    event_check = r.choice(event_list)
 
     # Sausage event, just lore
     # Converted
-    if "Sausage" == event_list[event_check] :
+    if "Sausage" == event_check:
+        lore = 1
         text_result = "You found a person digging through the trash for a sausage. Strange people these days."
         event_list.remove("Sausage")
+        
     # 9/11 event
-    elif "9/11" == event_list[event_check]:
+    # Converted
+    elif "9/11" == event_check:
         text_result = """You are chilling at the airport watching news as you wait for your next flight.
 You see on the news that an airport you were earlier got hit with a terrorist attack.
 The Shark is slowed down by this.
 """
         shark += 2
         event_list.remove("9/11")
+        
     # Mother event, Mother calls and gives money
     # Converted
-    elif "Mother" == event_list[event_check]:
+    elif "Mother" == event_check:
         money = r.randint(300,700)
+
         text_result = f"""Your mother is calling you.
         You pick up and she gives you some money
         You gain {money} """
@@ -52,10 +59,9 @@ The Shark is slowed down by this.
     # Organ seller event, You can sell your kidney
     # Needs to be completly changed
     # Converted to mysterious merchant
-    elif "mysterious merchant" == event_list[event_check]:
+    elif "mysterious merchant" == event_check:
         text_result = """While wandering through the airport, you stumble upon a shadowy figure.
         <br>The figure speaks in hushed tones, offering a deal too tempting to ignore."""
-        
         # Random chance for whether the player accepts or declines the deal
         outcome = r.choice(["accept", "decline"])
         
@@ -64,6 +70,7 @@ The Shark is slowed down by this.
             text_result += f"""<br>You instinctively agree, and the figure hands you an envelope containing {money}€.
             <br>Before you can ask any questions, they vanish into the crowd."""    
         else:
+            lore = 1
             text_result +="""<br>Something about the figure feels off, and you walk away without a second glance."
             <br>It might have been wise to trust your instincts."""
 
@@ -71,16 +78,17 @@ The Shark is slowed down by this.
         event_list.remove("mysterious merchant")
     
     # Russian roulette, The Shark calls you and offers to play a game. Ends game if russian roulette is played
-    elif "morpheus" == event_list[event_check]:
+    elif "morpheus" == event_check:
         text_result = """You receive a mysterious message from an unknown number.
         <br>The message reads: "I offer you a choice.<br>Take a pill to end your journey
         or continue to see how far you can run."
         <br>What do you do?"""
-        event_list.remove("Loanshark")
+        event_list.remove("morpheus")
+        result = {"event": True, "text": text_result, "input": "morpheus_pill"}
     
     # Celebrity event, he throws money around and you pick up some
     # Converted
-    elif "Celebrity" == event_list[event_check]:
+    elif "Celebrity" == event_check:
         money = r.randint(100, 1000)
         text_result = f"""You see a vaguely familiar looking celebrity.
 All of a sudden he starts throwing cash around.
@@ -91,8 +99,8 @@ You manage to pick up {money}€.
         
     # Burning hand event, loses money
     # Converted
-    elif "Burning hand" == event_list[event_check]:
-        money = r.randint(200,700)
+    elif "Burning hand" == event_check:
+        money = r.randint(-700,-200)
         text_result = f"""You burn your hand in an accident.
 The medical bills cost {money}€.
 Tough luck."""
@@ -101,26 +109,48 @@ Tough luck."""
     
     # Pool party event, loses money
     # Converted
-    elif "Poolparty" == event_list[event_check]:
-        money = r.randint(123,567)
+    elif "Poolparty" == event_check:
+        money = r.randint(-567,-123)
         text_result = f"""You are in a good mood.
 You decide to throw a pool party at the airports lounge.
 It cost you {money}€. """
+        event_list.remove("Poolparty")
 
 
     # P.Diddy offers you help
     # Converted
-    elif "PDiddy" == event_list[event_check]:
+    elif "PDiddy" == event_check:
+        money = r.randint(666, 1984)
         text_result = f"""You meet P.Diddy at the airport.
 He sees that you are troubled and offers to help you.
 You gain {money}.
 """
         event_list.remove("PDiddy")
+    elif "lost_ticket" == event_check:
+        carbon = r.randrange(100, 1000, 100)
+        text_result = f"""You found a lost airplane ticket on the ground.
+        <br>Out of the goodness of your heart, you decide to return it to the lost and found.
+        <br>As a reward, the airport staff give you a small sum of carbon points.
+        <br>You gain {carbon} carbon."""
+        event_list.remove("lost_ticket")
     # Insert new event above
     # end of random events
     else:
-        event_check = r.randint(0,len(event_list)-1)
-    return 
+        result = {"event": False, "text": "what the fuck", "event_choice": event_check}
+    
+    if money != 0:
+        result = {"event": True, "reward": money, "text": text_result}
+        #!#player.update_balance(money)
+    elif carbon !=0:
+        result = {"event": True, "reward": carbon, "text": text_result}
+        #!#player.update_carbon(carbon)
+    elif shark != 0:
+        result = {"event": True, "reward": shark, "text": text_result}
+        #!#player.update_shark(shark)
+    elif lore != 0:
+        result = {"event": True, "reward": "lore", "text": text_result}
+    
+    return jsonify(result)
 
 # with russian roulette being changed to morpheus pill, it is kind of pointless to have in the python.
 # might as well be in javascript but we'll see
@@ -234,19 +264,19 @@ Shark stares you in the eyes.
     return player_death
 
 #Event list
-event_list = ["Sausage",
-                "9/11",
-                "Mother",
-                "organ seller",
-                "Loanshark",
-                "Celebrity",
-                "Burning hand",
-                "Poolparty",
-                "PDiddy"]
+# event_list = ["Sausage",
+#                 "9/11",
+#                 "Mother",
+#                 "organ seller",
+#                 "Loanshark",
+#                 "Celebrity",
+#                 "Burning hand",
+#                 "Poolparty",
+#                 "PDiddy"]
 
 # Random event function, picks one event to be played out from the above list and then removes it from the list
 # Easy to add more events, To add more events add the name to the list above and make elif statement at the bottom of the script
-# elif "name_of_event" == event_list[event_check]:
+# elif "name_of_event" == event_check:
 def random_event():
     event_money = 0
     event_cp = 0
@@ -260,13 +290,13 @@ def random_event():
     event_check = r.randint(0,len(event_list)-1)
 
     # Sausage event, just lore
-    if "Sausage" == event_list[event_check] :
+    if "Sausage" == event_check :
         print("""You found a person digging through the trash for a sausage.
 Strange people these days.
 """)    
         event_list.remove("Sausage")
     # 9/11 event, needs content or to be changed to something else
-    elif "9/11" == event_list[event_check]:
+    elif "9/11" == event_check:
         print("""You are chilling at the airport watching news as you wait for your next flight.
 You see on the news that an airport you were earlier got hit with a terrorist attack.
 The Shark is slowed down by this.
@@ -274,7 +304,7 @@ The Shark is slowed down by this.
         shark += 2
         event_list.remove("9/11")
     # Mother event, Mother calls and gives money
-    elif "Mother" == event_list[event_check]:
+    elif "Mother" == event_check:
         print("""Your mother is calling you, do you wish to pick up?\n""")
         call_answer = input(print("Yes/No: ")).upper()
         while call_answer != "YES" and call_answer != "NO":
@@ -294,7 +324,7 @@ You got {mother_money}€ from your mother.
         event_list.remove("Mother")
 
     # Organ seller event, You can sell your kidney
-    elif "organ seller" == event_list[event_check]:
+    elif "organ seller" == event_check:
         print(f"""You meet an black market organ seller.
 He offers to buy your kidney
 Do you accept?
@@ -314,7 +344,7 @@ Do you accept?
         event_list.remove("organ seller")
     
     # Russian roulette, The Shark calls you and offers to play a game. Ends game if russian roulette is played
-    elif "Loanshark" == event_list[event_check]:
+    elif "Loanshark" == event_check:
         print("The loanshark is calling you, do you wish to pick up?\n")
         call_answer = input(print("Yes/No: ")).upper()
 
@@ -343,7 +373,7 @@ He gives you a choice.
         event_list.remove("Loanshark")
     
     # Celebrity event, he throws money around and you pick up some
-    elif "Celebrity" == event_list[event_check]:
+    elif "Celebrity" == event_check:
         celeb_money = r.randint(100, 1000)
         print(f"""You see a vaguely familiar looking celebrity.
 All of a sudden he starts throwing cash around.
@@ -352,7 +382,7 @@ You manage to pick up {celeb_money}€.
 """)
         event_money += celeb_money
         event_list.remove("Celebrity")
-    elif "Burning hand" == event_list[event_check]:
+    elif "Burning hand" == event_check:
         burning_money = r.randint(200,700)
         print(f"""You burn your hand in an accident.
 The medical bills cost {burning_money}€.
@@ -361,7 +391,7 @@ Tough luck.""")
         event_list.remove("Burning hand")
     
     # Pool party event, loses money
-    elif "Poolparty" == event_list[event_check]:
+    elif "Poolparty" == event_check:
         pool_money = r.randint(123,567)
         print(f"""You are in a good mood.
 You decide to throw a pool party at the airports lounge.
@@ -369,7 +399,7 @@ It cost you {pool_money}€. """)
         event_money -= pool_money
 
     # P.Diddy offers you help
-    elif "PDiddy" == event_list[event_check]:
+    elif "PDiddy" == event_check:
         print("""You meet P.Diddy at the airport.
 He sees that you are troubled and offers to help you.
 """)
