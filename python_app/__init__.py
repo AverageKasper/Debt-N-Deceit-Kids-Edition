fcd53ee91b4afa9a4811ea6df2cc51bd65bf4f63
Alexander Wolff <awfinland@gmail.com>
2024-12-07 17:37:57 +0200

    some small changes

--- python_app/__init__.py
+++ python_app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from python_app.routes import register_blueprints
+from python_app.routes.lollipop_task import lollipop_blueprint  # Import the lollipop blueprint
 
 def create_app():
     app = Flask(__name__)
@@ -8,5 +9,5 @@ def create_app():
 
     # Register blueprints
     register_blueprints(app)
-
+    app.register_blueprint(lollipop_blueprint, url_prefix='/lollipop')  # Register the lollipop blueprint
     return app
